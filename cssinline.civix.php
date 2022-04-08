188a994a919998d813d615457b26532fb4f80e32
Mark Hanna <mark@skvare.com>
2022-04-08 18:14:41 -0500

    fixing PHP 7.4 deprecation notice

--- cssinline.civix.php
+++ cssinline.civix.php
@@ -149,7 +149,7 @@ function _cssinline_civix_find_files($dir, $pattern) {
     if ($dh = opendir($subdir)) {
       while (FALSE !== ($entry = readdir($dh))) {
         $path = $subdir . DIRECTORY_SEPARATOR . $entry;
-        if ($entry{0} == '.') {
+        if ($entry[0] == '.') {
         } elseif (is_dir($path)) {
           $todos[] = $path;
         }
