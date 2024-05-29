cf085f9100fb069e6d6e404a61bacb2db1b42e4e
eileen <emcnaughton@wikimedia.org>
2024-05-30 07:05:51 +1200

    Run Civix

--- cssinline.php
+++ cssinline.php
@@ -29,15 +29,6 @@ function cssinline_civicrm_config(&$config) {
 }
 
 /**
- * Implementation of hook_civicrm_xmlMenu
- *
- * @param $files array(string)
- */
-function cssinline_civicrm_xmlMenu(&$files) {
-  _cssinline_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implementation of hook_civicrm_install
  */
 function cssinline_civicrm_install() {
@@ -45,58 +36,10 @@ function cssinline_civicrm_install() {
 }
 
 /**
- * Implementation of hook_civicrm_uninstall
- */
-function cssinline_civicrm_uninstall() {
-  return _cssinline_civix_civicrm_uninstall();
-}
-
-/**
  * Implementation of hook_civicrm_enable
  */
 function cssinline_civicrm_enable() {
   return _cssinline_civix_civicrm_enable();
-}
-
-/**
- * Implementation of hook_civicrm_disable
- */
-function cssinline_civicrm_disable() {
-  return _cssinline_civix_civicrm_disable();
-}
-
-/**
- * Implementation of hook_civicrm_upgrade
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- */
-function cssinline_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _cssinline_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implementation of hook_civicrm_managed
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- */
-function cssinline_civicrm_managed(&$entities) {
-  return _cssinline_civix_civicrm_managed($entities);
-}
-
-/**
- * Implementation of hook_civicrm_caseTypes
- *
- * Generate a list of case-types
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- */
-function cssinline_civicrm_caseTypes(&$caseTypes) {
-  _cssinline_civix_civicrm_caseTypes($caseTypes);
 }
 
 /**
